fc9217dad9afee161b67a9600db05a7c0dbb57bc
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-04-11 18:17:27 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
-　class BooksController < ApplicationController
+class BooksController < ApplicationController
     def new
     　@book = Book.new
     end
     def create
-    　@book = Book.new
-    　@book.save
-    　redirect_to book_path(book.id)
-    　end
+      book =Book.new(book_params)
+      book.save
+    　redirect_to 'books/'
+    end
     def index
       @books =Book.all
     end
@@ -16,4 +16,8 @@
     def edit
       @book = Book.find(params[:id])
     end
-  end
+    private
+    def book_params
+      params.require(:book).permit(:title,:body)
+    end
+end
