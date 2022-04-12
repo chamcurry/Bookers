02a23fc1860c5da1e1e61515a1fde661fd2b9cf4
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-04-12 03:30:36 +0000

    [pacth]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,18 +3,18 @@ class BooksController < ApplicationController
     　@book = Book.new
     end
     def create
-      book =Book.new(book_params)
-      book.save
-    　redirect_to 'books/'
+      @book =Book.new(book_params)
+      @book.save
+    　redirect_to 'books/:id'
     end
     def index
       @books =Book.all
     end
     def show
-      @book = Book.find(params[:id])
+      @book =Book.find(params[:id])
     end
     def edit
-      @book = Book.find(params[:id])
+      @book =Book.find(params[:id])
     end
     private
     def book_params
