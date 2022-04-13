d3c6c6c65af273d6a871d0cbf95e29c2c2f651ef
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-04-13 18:18:34 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,11 @@ class BooksController < ApplicationController
     end
     def create
       @book =Book.new(book_params)
-      @book.save
+      if @book.save
      redirect_to book_path(@book.id)
+      else
+     render :index
+      end
     end
     def index
       @books =Book.all
